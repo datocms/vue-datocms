16b7be77715140a11a8749d98e996733d0501c24
Stefano Verna <s.verna@datocms.com>
2021-04-22 08:31:31 +0200

    Test different build method

---
+++ rollup.config.js
@@ -0,0 +1,58 @@
+import dts from 'rollup-plugin-dts'
+import esbuild from 'rollup-plugin-esbuild'
+import tsConfig from './tsconfig.json'
+import pkgJson from './package.json'
+
+const dist = 'dist'
+const name = 'VueDatocms'
+const globals = { 'vue-demi': 'VueDemi'  }
+
+const baseConfig = {
+  input: 'src/index.ts',
+  external: [...Object.keys(pkgJson.peerDependencies), ...Object.keys(pkgJson.dependencies)],
+  plugins: [
+    esbuild({
+      target: tsConfig.compilerOptions.target,
+      minify: false,
+    }),
+  ],
+}
+
+export default [
+  {
+    input: baseConfig.input,
+    output: {
+      file: dist + '/index.d.ts',
+    },
+    plugins: [dts()],
+  },
+  {
+    ...baseConfig,
+    output: {
+      file: dist + '/index.esm.js',
+      format: 'esm',
+    },
+  },
+  {
+    ...baseConfig,
+    output: {
+      name,
+      globals,
+      file: dist + '/index.umd.js',
+      format: 'umd',
+      exports: 'named',
+      compact: true,
+    },
+  },
+  {
+    ...baseConfig,
+    output: {
+      name,
+      globals,
+      file: dist + '/index.min.js',
+      format: 'iife',
+      exports: 'named',
+      compact: true,
+    },
+  },
+]
