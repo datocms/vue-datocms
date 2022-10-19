f134cdfeb26b9d500ac64476d74c327e556a61dc
Silvano Stralla <silvano.stralla@sistrall.it>
2022-10-19 11:07:50 +0200

    Add a note on why we need a different config for production and development.

--- examples/site-search/vite.config.ts
+++ examples/site-search/vite.config.ts
@@ -12,6 +12,13 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['vue-demi']
   },
+  // This example is also deployed in production as a demo. So, we need 
+  // to sligthly tweak the setup:
+  // 
+  // - in development mode, we need to resolve to the grand-parent folder,
+  //   where the version of vue-datocms under development currently lives;
+  // - in production, instead, vue-datocms is actually a normal dependency,
+  //   so we don't need any special setup here.
   ...(process.env.NODE_ENV !== 'production' && {
     resolve: {
       alias: {
